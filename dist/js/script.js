0213fc48a8254f1c4bb7e39e44cb7c2f785f4e17
VKhvesyk <khvesyk.vasyl@gmail.com>
2021-09-29 14:25:06 +0300

    Added API for get weather data and to get image by city name. Also aded function for convert time and random number

---
+++ dist/js/script.js
@@ -0,0 +1,177 @@
+'use strict';
+
+const btn = document.querySelector('#btn'),
+      input = document.querySelector('#userInput'),
+      body = document.querySelector('body'),
+      currentTemp = document.querySelector('#currentTemp'),
+      cityName = document.querySelector('#cityName'),
+      currentTime = document.querySelector('#currentTime'),
+      currentDate = document.querySelector('#currentDate'),
+      weatherIcon = document.querySelector('.main-wrapper__weather__icon'),
+      weatherIndexes = document.querySelector('.main-wrapper__weather__indexes'),
+      visualBlock = document.querySelector('.main-wrapper__weather__visual');
+
+let data,
+    photo;
+
+
+function timeConverter(UNIXtimestamp, selector){
+    let a = new Date(UNIXtimestamp * 1000);
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let hour = a.getHours();
+    let min = a.getMinutes();
+    let sec = a.getSeconds();
+    let dateNow = date + ' ' + month + ' ' + year;
+    let time = hour + ':' + min;
+
+    if (selector == 'time') {
+        return time;
+    } else {
+        return dateNow;
+    }
+}
+
+function randomInteger(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+    }
+
+
+
+function insertValue() {
+    cityName.innerText = `${data.name}`;
+    currentTime.innerText = `${timeConverter(data.dt, 'time')}`;
+    currentDate.innerText = `${timeConverter(data.dt, 'date')}`;
+    currentTemp.innerHTML = `${Math.round(data.main.temp - 273.15)}&degC`;
+    weatherIcon.innerHTML = `
+    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="weather icon"></img>
+    <p id="weatherDescr">${data.weather[0].description}</p>
+    `;
+
+    weatherIndexes.innerHTML = `
+    <div class="index">
+        <div>Current temp: </div>
+        <div>${Math.round(data.main.temp - 273.15)} &degC</div>
+    </div>
+
+        <span></span>
+
+    <div class="index">
+        <div>Current pressure: </div>
+        <div>${Math.round(data.main.pressure * 0.75006375541921)} mmHg</div>
+    </div>
+
+        <span></span>
+
+    <div class="index">
+        <div>Current humidity: </div>
+        <div>${Math.round(data.main.humidity)} %</div>
+    </div>
+
+        <span></span>
+
+    <div class="index">
+        <div>Min temperature: </div>
+        <div>${Math.round(data.main.temp_min - 273.15)} &degC</div>
+    </div>
+
+        <span></span>
+
+    <div class="index">
+        <div>Max temperature: </div>
+        <div>${Math.round(data.main.temp_max - 273.15)} &degC</div>
+    </div>
+    `;
+}
+
+
+function collectData(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=c399362e0b13104407ba21b549d95547`)
+        .then(response => {
+        data = response.json();
+        return data;
+        })
+        .then(result => {
+            data = result;
+            return data;
+        })
+        .then(() => {
+            insertValue();
+        })
+        .catch(() => {
+            console.log('Сталась помилка. Наші котики вже працюють над цим');
+        });
+
+    fetch(`https://api.unsplash.com/search/photos?client_id=qN4OKFJMhjsIhlHWXBftZ2Pdasy9WLvSQ3NAVZ7bslo&?page=1&query=${cityName}`)
+        .then(response => {
+        photo = response.json();
+        return photo;
+        })
+        .then(result => {
+            photo = result;
+            return photo;
+        })
+        .then(() => {
+            console.log(photo);
+            visualBlock.style.cssText = `background: no-repeat center / cover url(${photo.results[randomInteger(0, 9)].urls.regular})`;
+        })
+        .catch(() => {
+            console.log('Сталась помилка. Наші котики вже працюють над цим');
+        });
+}
+
+
+collectData('Kyiv');
+
+
+
+btn.addEventListener('click', () => {
+
+    collectData(input.value);
+    insertValue();
+
+});
+
+
+// const input = document.getElementById('userInput');
+//     let options = {
+//         types: ['(cities)']
+//     };
+
+//     autocomplete = new google.maps.places.Autocomplete(input, options);
+
+
+
+
+// let url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+// let token = "9fa926bbff01ae7b932ab8cc3a78d262dca3f622";
+// let query = "Дніпр";
+
+// let options = {
+//     method: "POST",
+//     mode: "cors",
+//     count: 3,
+//     locations: [
+//         {
+//             country: "*"
+//         }
+//     ],
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//         "Authorization": "Token " + token
+//     },
+//     body: JSON.stringify({query: query})
+// };
+
+// fetch(url, options)
+// .then(response => response.json())
+// .then(result => console.log(result))
+// .catch(error => console.log("error", error));
+
+
+
+
